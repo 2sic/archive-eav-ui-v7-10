544e0cc43fc8afeccd0ab3625995c9f3eedc57fd
BeniGemperle <Benjamin.Gemperle@2sic.com>
2014-03-12 19:11:09 +0100

    Added missing files

---
+++ EAVManagementUI-Project/Common/EventHandlers.cs
@@ -0,0 +1,5 @@
+﻿namespace ToSic.Eav.ManagementUI
+{
+	public delegate void EntityUpdatedEventHandler(Entity newEntity);
+	public delegate void EntityDeletingEventHandler(EntityDeletingEventArgs entityDeletingEventArgs);
+}
