6949d4f4a727079cf6f0d6c8907973c2e4f17e5b
iJugleboy <daniel.mettler@2sic.com>
2015-06-02 23:23:13 +0200

    Added / moved all import code to EAV Also includes some Resx, might cause issues

---
+++ ToSic.Eav/ImportExport/Refactoring/DocumentNodeNames.cs
@@ -0,0 +1,18 @@
+﻿namespace ToSic.Eav.ImportExport.Refactoring
+{
+    /// <summary>
+    /// Element names of the 2SexyContent data xml.
+    /// </summary>
+    internal static class DocumentNodeNames
+    {
+        public const string Root = "SexyContentData";
+
+        public const string Entity = "Entity";
+
+        public const string EntityGuid = "Guid";
+
+        public const string EntityLanguage = "Language";
+
+        public const string RootTypeAttribute = "Type";
+    }
+}
