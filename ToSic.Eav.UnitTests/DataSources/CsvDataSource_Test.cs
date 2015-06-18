7f5b9279863756265c1a3da1f7274bee0e59a0bc
tobias2sic <tobias.krattinger@2sic.com>
2015-06-18 09:26:25 +0200

    CsvDataSource added.

---
+++ ToSic.Eav.UnitTests/DataSources/CsvDataSource_Test.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToSic.Eav.DataSources;
+using ToSic.Eav.UnitTests.ValueProvider;
+
+namespace ToSic.Eav.UnitTests.DataSources
+{
+    [TestClass]
+    public class CsvDataSource_Test
+    {
+        private const int TestFileRowCount = 40;
+
+        private const int TestFileColumnCount = 5;
+
+        private const int TestFileIdColumnIndex = 0;
+
+        private const int TestFileTitleColumnIndex = 1;
+
+
+        [TestMethod]
+        public void CsvDataSource_ParseSemicolonDelimitedFile()
+        {
+            var source = CreateDataSource("Files/CsvDataSource - Test Semicolon Delimited.csv", ";", "Anonymous", TestFileIdColumnIndex, TestFileTitleColumnIndex);
+            var sourceList = source.LightList;
+
+            AssertIsSourceListValid(sourceList, TestFileIdColumnIndex, TestFileTitleColumnIndex);
+        }
+
+
+        [TestMethod]
+        public void CsvDataSource_ParseTabDelimitedFile()
+        {
+            var source = CreateDataSource("Files/CsvDataSource - Test Tab Delimited.csv", "\t", "Anonymous", TestFileIdColumnIndex, TestFileTitleColumnIndex);
+            var sourceList = source.LightList;
+
+            AssertIsSourceListValid(sourceList, TestFileIdColumnIndex, TestFileTitleColumnIndex);
+        }
+
+        [TestMethod]
+        public void CsvDataSource_ParseFileWithQuotedText()
+        {
+            var source = CreateDataSource("Files/CsvDataSource - Test Quoted Text.csv", ";", "Anonymous", TestFileIdColumnIndex, TestFileTitleColumnIndex);
+            var sourceList = source.LightList;
+
+            AssertIsSourceListValid(sourceList, TestFileIdColumnIndex, TestFileTitleColumnIndex);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CsvDataSource_ParseFileWithInvalidIdColumnIndex100()
+        {
+            try
+            {
+                var source = CreateDataSource("Files/CsvDataSource - Test Semicolon Delimited.csv", ";", "Anonymous", 100, TestFileTitleColumnIndex);
+                var sourceList = source.LightList;
+            }
+            catch (Exception ex)
+            {       
+                // The pipeline does wrap my exception expected
+                throw ex.InnerException;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CsvDataSource_ParseFileWithInvalidTextColumnIndex100()
+        {
+            try
+            {
+                var source = CreateDataSource("Files/CsvDataSource - Test Semicolon Delimited.csv", ";", "Anonymous", TestFileIdColumnIndex, 100);
+                var sourceList = source.LightList;
+            }
+            catch (Exception ex)
+            {       
+                // The pipeline does wrap my exception expected
+                throw ex.InnerException;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void CsvDataSource_ParseFileWithInvalidRow()
+        {
+            try
+            {
+                var source = CreateDataSource("Files/CsvDataSource - Test Invalid Row.csv", ";", "Anonymous", TestFileIdColumnIndex, TestFileTitleColumnIndex);
+                var sourceList = source.LightList;
+            }
+            catch (Exception ex)
+            {
+                // The pipeline does wrap my exception expected
+                throw ex.InnerException;
+            }
+        }
+
+
+        private static void AssertIsSourceListValid(IEnumerable<IEntity> sourceList, int idColumnIndex, int titleColumnIndex)
+        {
+            // List
+            Assert.AreEqual(sourceList.Count(), TestFileRowCount, "Entity list has not the expected length.");
+
+            // Entities
+            for (var i = 0; i < sourceList.Count(); i++)
+            {
+                var entity = sourceList.ElementAt(i);
+                
+                Assert.AreEqual(TestFileColumnCount, entity.Attributes.Count(), "Entity " + i + ": Attributes do not match the columns in the file.");
+                Assert.AreEqual(GetAttributeValueAt(entity, idColumnIndex), entity.EntityId.ToString(), "Entity " + i + ": ID does not match.");
+                Assert.IsNotNull(GetAttributeValueAt(entity, titleColumnIndex), "Entity " + i + ": Title should not be null.");
+            }
+        }
+
+        private static object GetAttributeValueAt(IEntity entity, int index)
+        {
+            return entity.GetBestValue(entity.Attributes.ElementAt(index).Key);
+        }
+
+        public static CsvDataSource CreateDataSource(string filePath, string delimiter = ";", string contentType = "Anonymous", int IdColumnIndex = 0, int TitleColumnIndex = 1)
+        {
+            var source = new CsvDataSource() 
+            {
+                FilePath = filePath,
+                Delimiter = delimiter,
+                ContentType = contentType,
+                IdColumnIndex = IdColumnIndex,
+                TitleColumnIndex = TitleColumnIndex
+
+            };
+            source.ConfigurationProvider = new ValueCollectionProvider_Test().ValueCollection();
+            return source;
+        }
+    }
+}
---
+++ ToSic.Eav/DataSources/CsvDataSource.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic.FileIO;
+
+
+namespace ToSic.Eav.DataSources
+{
+    [PipelineDesigner]
+    public class CsvDataSource : BaseDataSource
+    {
+        private const string FilePathKey = "FilePath";
+
+        public string FilePath
+        {
+            get { return Configuration[FilePathKey]; }
+            set { Configuration[FilePathKey] = value; }
+        }
+
+
+        private const string DelimiterKey = "Delimiter";
+
+        public string Delimiter
+        {
+            get { return Configuration[DelimiterKey]; }
+            set { Configuration[DelimiterKey] = value; }
+        }
+
+
+        private const string ContentTypeKey = "ContentType";
+
+        public string ContentType
+        {
+            get { return Configuration[ContentTypeKey]; }
+            set { Configuration[ContentTypeKey] = value; }
+        }
+
+
+        private const string IdColumnIndexKey = "IdColumnIndex";
+
+        public int IdColumnIndex
+        {
+            get { return int.Parse(Configuration[IdColumnIndexKey]); }
+            set { Configuration[IdColumnIndexKey] = value.ToString(); }
+        }
+
+
+        private const string TitleColumnIndexKey = "TitleColumnIndex";
+
+        public int TitleColumnIndex
+        {
+            get { return int.Parse(Configuration[TitleColumnIndexKey]); }
+            set { Configuration[TitleColumnIndexKey] = value.ToString(); }
+        }
+
+
+        public CsvDataSource()
+        {
+            Out.Add(DataSource.DefaultStreamName, new DataStream(this, DataSource.DefaultStreamName, null, GetList));
+
+            Configuration.Add(FilePathKey, "[Settings:FilePath]");
+            Configuration.Add(DelimiterKey, "[Settings:Delimiter||;]");
+            Configuration.Add(ContentTypeKey, "[Settings:ContentType||Anonymous]");
+            Configuration.Add(IdColumnIndexKey, "[Settings:IdColumnIndex||0]");
+            Configuration.Add(TitleColumnIndexKey, "[Settings:TitleColumnIndex||0]");
+            CacheRelevantConfigurations = new[] { FilePathKey, DelimiterKey, ContentTypeKey };
+        }
+
+
+        private IEnumerable<IEntity> GetList()
+        {
+            EnsureConfigurationIsLoaded();
+
+            var entityList = new List<IEntity>();
+
+            using (var parser =  new TextFieldParser(FilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.TrimWhiteSpace = true;
+                parser.Delimiters = new string[] { Delimiter };
+
+                // Parse header
+                var columns = parser.ReadFields();
+                if (IdColumnIndex < 0 || IdColumnIndex >= columns.Length)
+                    throw new ArgumentException("Index for ID column is out of range.", "IdColumnIndex");
+
+                if (TitleColumnIndex < 0 || TitleColumnIndex >= columns.Length)
+                    throw new ArgumentException("Index for Title column is out of range.", "TitleColumnIndex");
+
+                // Parse data
+                while (!parser.EndOfData) 
+                {
+                    var fields = parser.ReadFields();
+                    if (fields.Length != columns.Length)
+                        throw new FormatException("Row " + parser.LineNumber + ": The number of fields does not match the column count.");
+
+                    int entityId;
+                    if (!int.TryParse(fields[IdColumnIndex], out entityId))
+                    {
+                        entityId = (int)parser.LineNumber;
+                    }
+                    var entityValues = new Dictionary<string, object>();
+                    for (var i = 0; i < columns.Length; i++)
+                    {
+                        entityValues.Add(columns[i], fields[i]);
+                    }
+
+                    entityList.Add(new Data.Entity(entityId, ContentType, entityValues, columns[TitleColumnIndex]));
+                }
+            }
+            return entityList;
+        }
+    }
+}
