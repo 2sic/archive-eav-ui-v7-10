e4bc62e6b375e4891e0b15791c1e575c0c8fde2a
iJugleboy <daniel.mettler@2sic.com>
2015-10-05 13:46:43 +0200

    merged, now with uiDebug etc.

--- ToSic.Eav.ManagementUI/src/admin/services/uiDebug.js
+++ ToSic.Eav.ManagementUI/src/admin/services/uiDebug.js
@@ -2,6 +2,12 @@
 // will add
 // vm.debug -> shows if in debug mode - bind ng-ifs to this
 // vm.maybeEnableDebug - a method which checks for ctrl+shift-click and if yes, changes debug state
+//
+// How to use
+// 1. add uiDebug to your controller dependencies like:    contentImportController(appId, ..., uiDebug, $modalInstance, $filter)
+// 2. add a line after creating your vm-object like:       uiDebug.extendViewModel(vm);
+// 3. add a click event as far out as possible on html:    <div ng-click="vm.maybeEnableDebug($event)">
+// 4. wrap your hidden stuff in an ng-if:                  <div ng-if="vm.debug">
 
 angular.module("EavServices")
     .factory("uiDebug", function () {
