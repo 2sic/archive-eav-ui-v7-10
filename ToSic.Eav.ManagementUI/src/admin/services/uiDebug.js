7d6c8a15ad3f36085871e48261e90e8d684e9858
iJugleboy <daniel.mettler@2sic.com>
2015-10-05 13:23:55 +0200

    added a standardized debugging feature for views...

---
+++ ToSic.Eav.ManagementUI/src/admin/services/uiDebug.js
@@ -0,0 +1,21 @@
+﻿// uiDebug
+// will add
+// vm.debug -> shows if in debug mode - bind ng-ifs to this
+// vm.maybeEnableDebug - a method which checks for ctrl+shift-click and if yes, changes debug state
+
+angular.module("EavServices")
+    .factory("uiDebug", function () {
+        var svc = {};
+
+        svc.extendViewModel = function extendViewModel(viewModel) {
+            viewModel.debug = false;
+            viewModel.maybeEnableDebug = function maybeEnableDebug(evt) {
+                evt = window.event || evt;
+                var ctrlAndShiftPressed = evt.ctrlKey && evt.shiftKey;
+                if (ctrlAndShiftPressed)
+                    viewModel.debug = !viewModel.debug;
+            };
+        };
+
+        return svc;
+    });
