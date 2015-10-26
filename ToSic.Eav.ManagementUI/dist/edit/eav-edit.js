dc8d5050690e698238a6c6536e49e7c534898c77
iJugleboy <daniel.mettler@2sic.com>
2015-10-26 06:57:14 +0100

    fixed bug because of temporary code...

--- ToSic.Eav.ManagementUI/dist/edit/eav-edit.js
+++ ToSic.Eav.ManagementUI/dist/edit/eav-edit.js
@@ -5,37 +5,12 @@
     "use strict";
     var module = angular.module("eavCustomFields", ["oc.lazyLoad"])
 
-        // here's a special trick from http://slides.com/gruizdevilla/late-registering-and-lazy-load-in-angularjs-en#/5
-        //.config(
-        //    function(formlyConfigProvider, defaultFieldWrappers,
-        //        $controllerProvider,
-        //        $compileProvider,
-        //        $filterProvider,
-        //        $provide
-        //    ) {
-        //        module.IsLazyLoader = true;
-        //        module.formlyConfigProvide = formlyConfigProvider;
-        //        module.defaultFieldWrappers = defaultFieldWrappers;
-                 
-                 
-        //        //module.controller = $controllerProvider.register;
-        //        //module.directive = $compileProvider.directive;
-        //        //module.filter = $filterProvider.register;
-        //        //module.factory = $provide.factory;
-        //        //module.service = $provide.service;
-
-        //        //    // added by 2dm
-        //        //module.config = $provide.config;
-        //    })
     .config(["$ocLazyLoadProvider", function ($ocLazyLoadProvider) {
         $ocLazyLoadProvider.config({
             debug: true
         });
     }]);
 
-    //module.run(function(formlyConfig) {
-    //    module.formlyConfig = formlyConfig;
-    //});
 })();
 /* Main object with dependencies, used in wrappers and other places */
 (function () {
