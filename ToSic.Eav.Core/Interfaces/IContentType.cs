535937b052acad5854b7255c7c6ed7dbfca324e7
Raphael Müller <raaphii@gmail.com>
2015-11-09 14:25:25 +0100

    Fixed merge issue

--- ToSic.Eav.Core/Interfaces/IContentType.cs
+++ ToSic.Eav.Core/Interfaces/IContentType.cs
@@ -44,10 +44,10 @@ namespace ToSic.Eav
         /// </summary>
         int? UsesConfigurationOfAttributeSet { get; }
 
-        /// <summary>
-        /// Dictionary with all AttributeHelperTools Definitions
-        /// </summary>
-        IDictionary<int, AttributeBase> AttributeDefinitions { get; set; }
+        ///// <summary>
+        ///// Dictionary with all AttributeHelperTools Definitions
+        ///// </summary>
+        //IDictionary<int, AttributeBase> AttributeDefinitions { get; set; }
 
 	}
 }
