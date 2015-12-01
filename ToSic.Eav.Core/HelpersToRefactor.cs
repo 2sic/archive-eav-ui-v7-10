5a7a307c238f8923608aa819f276dfe439a83b46
Raphael Müller <raaphii@gmail.com>
2015-12-01 15:46:50 +0100

    Fixed issue where an invalid XML character caused issues when saving a value (replace all invalid XML characters from serialized string)

--- ToSic.Eav.Core/HelpersToRefactor.cs
+++ ToSic.Eav.Core/HelpersToRefactor.cs
@@ -22,9 +22,23 @@ namespace ToSic.Eav
                 newValueSerialized = string.Empty;
             else
                 newValueSerialized = newValue.ToString();
+            newValueSerialized = CleanInvalidXmlChars(newValueSerialized);
             return newValueSerialized;
         }
 
-
+        /// <summary>
+        /// Because entities are converted to XML (for the history / timeline table), all values
+        /// need to be convertable to XML. Removes invalid characters from a string.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static string CleanInvalidXmlChars(string text)
+        {
+            // From xml spec valid chars: 
+            // #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]     
+            // any Unicode character, excluding the surrogate blocks, FFFE, and FFFF.
+            string re = @"[^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]";
+            return System.Text.RegularExpressions.Regex.Replace(text, re, "");
+        }
     }
 }
