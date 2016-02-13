224e1f77ae594004b5197628c2a2b0a70a24bf2f
iJugleboy <daniel.mettler@2sic.com>
2016-02-13 15:18:58 +0100

    improved ctrlS - save service to properly bind/unbind - close #34

--- src/admin/services/ctrl-save.service.js
+++ src/admin/services/ctrl-save.service.js
@@ -1,20 +1,48 @@
-﻿angular.module("EavServices")
+﻿/* 
+ * 
+ * Simple service which takes care of ctrl+S keyboard shortcuts. 
+ * use it as a service for your controller, then add a line like 
+         function activate() {
+            // add ctrl+s to save
+            ctrlS.bind(function() { vm.save(false); });
+        }
+
+ */
+
+angular.module("EavServices")
     .factory("ctrlS", function ($window) {
         var save = {
             _event: null,
+            _action: null,
+            _isbound: false,
+
+            // this will be called on each keydown, will check if it was a ctrl+S
+            detectCtrlSAndExcecute: function(e)  {
+                if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+                    e.preventDefault();
+                    save._action();
+                }
+            },
 
             bind: function bind(action) {
-                save._event = window.addEventListener("keydown", function (e) {
-                    if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-                        e.preventDefault();
-                        action();
-                    }
-                }, false);
+                save._action = action;
+                save._isbound = true;
+                save._event = $window.addEventListener("keydown", save.detectCtrlSAndExcecute, false);
 
             },
 
             unbind: function unbind() {
-                window.removeEventListener(save._event);
+                $window.removeEventListener("keydown", save.detectCtrlSAndExcecute);
+                save._isbound = false;
+            },
+
+            // re-attach Ctrl+S if it had already been attached previously
+            rebind: function rebind() {
+                if (save._action === null)
+                    throw "can't rebind, as it was never initially bound";
+                if (!save._isbound)
+                    throw "can't rebind, as it's still bound";
+                save.bind(save._action);
             }
         };
 
