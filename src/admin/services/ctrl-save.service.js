5a75600c69a38282748f8f6b7f18a8b834f87150
iJugleboy <daniel.mettler@2sic.com>
2016-02-13 23:57:27 +0100

    Implemented Ctrl+S to be re-usable across systems by changing it from singleton to a factory = close #35

--- src/admin/services/ctrl-save.service.js
+++ src/admin/services/ctrl-save.service.js
@@ -11,40 +11,50 @@
 
 angular.module("EavServices")
     .factory("ctrlS", function ($window) {
-        var save = {
-            _event: null,
-            _action: null,
-            _isbound: false,
 
-            // this will be called on each keydown, will check if it was a ctrl+S
-            detectCtrlSAndExcecute: function(e)  {
-                if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-                    e.preventDefault();
-                    save._action();
+        // Create a capture Ctrl+S and execute action-object
+        function createSave(action) {
+            var save = {
+                _event: null,
+                _action: null,
+                _isbound: false,
+
+                // this will be called on each keydown, will check if it was a ctrl+S
+                detectCtrlSAndExcecute: function(e) {
+                    if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+                        if (save._action === null)
+                            return console.log("can't do anything on ctrl+S, no action registered");
+                        e.preventDefault();
+                        save._action();
+                    }
+                },
+
+                bind: function bind(action) {
+                    save._action = action;
+                    save._isbound = true;
+                    save._event = $window.addEventListener("keydown", save.detectCtrlSAndExcecute, false);
+
+                },
+
+                unbind: function unbind() {
+                    $window.removeEventListener("keydown", save.detectCtrlSAndExcecute);
+                    save._isbound = false;
+                },
+
+                // re-attach Ctrl+S if it had already been attached previously
+                rebind: function rebind() {
+                    if (save._action === null)
+                        throw "can't rebind, as it was never initially bound";
+                    if (!save._isbound)
+                        throw "can't rebind, as it's still bound";
+                    save.bind(save._action);
                 }
-            },
+            };
 
-            bind: function bind(action) {
-                save._action = action;
-                save._isbound = true;
-                save._event = $window.addEventListener("keydown", save.detectCtrlSAndExcecute, false);
+            save.bind(action);
 
-            },
+            return save;
+        }
 
-            unbind: function unbind() {
-                $window.removeEventListener("keydown", save.detectCtrlSAndExcecute);
-                save._isbound = false;
-            },
-
-            // re-attach Ctrl+S if it had already been attached previously
-            rebind: function rebind() {
-                if (save._action === null)
-                    throw "can't rebind, as it was never initially bound";
-                if (!save._isbound)
-                    throw "can't rebind, as it's still bound";
-                save.bind(save._action);
-            }
-        };
-
-        return save;
+        return createSave;
     });
