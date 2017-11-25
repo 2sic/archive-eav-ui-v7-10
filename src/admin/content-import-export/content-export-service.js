eb41c6c924e3f0c3a6ef01030b408b3c30915d65
iJungleboy <daniel.mettler@2sic.com>
2017-11-25 11:05:41 +0100

    added hidden feature to export json-types - part of https://github.com/2sic/2sxc/issues/1393

--- src/admin/content-import-export/content-export-service.js
+++ src/admin/content-import-export/content-export-service.js
@@ -1,29 +1,38 @@
 ﻿(function () {
 
-    angular.module("ContentExportApp")
-         .factory("contentExportService", contentExportService);
+    angular.module('ContentExportApp')
+         .factory('contentExportService', contentExportService);
 
 
     function contentExportService($http, eavConfig) {
-        var srvc = {
-            exportContent: exportContent
+        return {
+            exportContent: exportContent,
+            exportJson: exportJson
         };
-        return srvc;
 
         function exportContent(args, selectedIds) {
-            var url = eavConfig.getUrlPrefix("api") + "/eav/ContentExport/ExportContent",
-                addids = selectedIds ? "&selectedids=" + selectedIds.join() : "",
+            var url = eavConfig.getUrlPrefix('api') + '/eav/ContentExport/ExportContent',
+                addids = selectedIds ? '&selectedids=' + selectedIds.join() : '',
                 fullUrl = url
-                    + "?appId=" + args.AppId
-                    + "&language=" + args.Language
-                    + "&defaultLanguage=" + args.DefaultLanguage
-                    + "&contentType=" + args.ContentType
-                    + "&recordExport=" + args.RecordExport
-                    + "&resourcesReferences=" + args.ResourcesReferences
-                    + "&languageReferences=" + args.LanguageReferences
+                    + '?appId=' + args.AppId
+                    + '&language=' + args.Language
+                    + '&defaultLanguage=' + args.DefaultLanguage
+                    + '&contentType=' + args.ContentType
+                    + '&recordExport=' + args.RecordExport
+                    + '&resourcesReferences=' + args.ResourcesReferences
+                    + '&languageReferences=' + args.LanguageReferences
                     + addids;
 
-            window.open(fullUrl, "_blank" /* "_self" */, "");
+            window.open(fullUrl, '_blank', '');
+        }
+
+        function exportJson(appId, typeName) {
+            var url = eavConfig.getUrlPrefix('api')
+                + '/eav/ContentExport/DownloadTypeAsJson'
+                + '?appId=' + appId
+                + '&name=' + typeName;
+
+            window.open(url, '_blank', '');
         }
     }
 }());
