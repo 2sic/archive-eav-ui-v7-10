3d2de6ce1daa22b9355ba42dee1a41128b3fc18e
2sic <info@2sxc.org>
2017-11-30 14:24:32 +0100

    Inherit from entity-default field template to get rid of duplicate code

--- src/edit/fields/entity/entity-query.js
+++ src/edit/fields/entity/entity-query.js
@@ -1,6 +1,5 @@
 ﻿/* 
- * Field: Entity - Default
- * Also contains much business logic and the necessary controller
+ * Field: Entity - Query
  * 
  */
 
@@ -17,62 +16,19 @@ angular.module("eavFieldTemplates")
             controller: "FieldTemplate-EntityQueryCtrl"
         });
     })
-    .controller("FieldTemplate-EntityQueryCtrl", function ($scope, $http, $filter, $translate, $uibModal, appId, eavAdminDialogs, eavDefaultValueService, fieldMask, $q, $timeout, entitiesSvc, debugState, query) {
-        
-        var contentType, paramsMask, lastParamsMask;
+    .controller("FieldTemplate-EntityQueryCtrl", function ($controller, $scope, $http, $filter, $translate, $uibModal, appId, eavAdminDialogs, eavDefaultValueService, fieldMask, $q, $timeout, entitiesSvc, debugState, query) {
+
+        // use "inherited" controller just like described in http://stackoverflow.com/questions/18461263/can-an-angularjs-controller-inherit-from-another-controller-in-the-same-module
+        $controller("FieldTemplate-EntityCtrl", { $scope: $scope });
+
+        var paramsMask, lastParamsMask;
 
         function activate() {
-            // ensure settings are merged
-            if (!$scope.to.settings.merged)
-                $scope.to.settings.merged = {};
-
-            // of no real data-model exists yet for this value (list of chosen entities), then create a blank
-            if ($scope.model[$scope.options.key] === undefined || $scope.model[$scope.options.key].Values[0].Value === "") {
-                var initVal = eavDefaultValueService($scope.options);   // note: works for simple entries as well as multiple, then it has to be an array though
-                $scope.model[$scope.options.key] = { Values: [{ Value: initVal, Dimensions: {} }]};
-            }
-
-            // create short names for template
-            var valList = $scope.model[$scope.options.key].Values[0].Value;
-
-            $scope.chosenEntities = valList;
-            $scope.selectedEntity = null;
-
             // Initialize url parameters mask
-            paramsMask = fieldMask($scope.to.settings.merged.UrlParameters || null, $scope, $scope.maybeReload, null);// this will contain the auto-resolve url parameters
-
-            // Initialize content type mask
-            contentType = fieldMask($scope.to.settings.merged.EntityType || null, $scope, $scope.maybeReload, null);// this will contain the auto-resolve type (based on other contentType-field)
-
-            $scope.availableEntities = [];
+            paramsMask = fieldMask($scope.to.settings.merged.UrlParameters || null, $scope, $scope.maybeReload, null); // this will contain the auto-resolve url parameters
         }
-
-        $scope.debug = debugState;
-
-        // add an just-picked entity to the selected list
-        $scope.addEntity = function(item) {
-            if (item === null) return false;
-            $scope.chosenEntities.push(item);
-            $scope.selectedEntity = null;
-            return true;
-        };
-
-        // open the dialog for a new item
-        $scope.openNewEntityDialog = function () {
-            function reloadAfterAdd(result) {
-                if (!result || result.data === null || result.data === undefined)
-                    return;
-
-                $scope.maybeReload(true).then(function () {
-                    $scope.chosenEntities.push(Object.keys(result.data)[0]);
-                    setDirty();
-                });
-            }
-            eavAdminDialogs.openItemNew(contentType.resolve(), reloadAfterAdd);
-        };
-
-        // ajax call to get all entities
-        // todo: move to a service some time + enhance to provide more fields if needed
+        
+        // ajax call to get the entities
         $scope.getAvailableEntities = function () {
             var params = paramsMask.resolve(); // always get the latest definition
             return query("Test?includeGuid=true" + (params ? '&' + params : '')).get().then(function (data) {
@@ -91,78 +47,5 @@ angular.module("eavFieldTemplates")
             return $q.when();
         };
 
-        // get a nice label for any entity, including non-existing ones
-        $scope.getEntityText = function (entityId) {
-            if (entityId === null)
-                return "empty slot"; // todo: i18n
-            var entities = $filter("filter")($scope.availableEntities, { Value: entityId });
-            return entities.length > 0 ? entities[0].Text : $translate.instant("FieldType.Entity.EntityNotFound"); 
-        };
-
-        // remove needs the index --> don't name "remove" - causes problems
-        $scope.removeSlot = function(itemGuid, index) {
-            $scope.chosenEntities.splice(index, 1);
-            setDirty();
-        };
-
-        $scope.deleteItemInSlot = function (itemGuid, index) {
-            if ($scope.to.settings.merged.EntityType === '') {
-                alert('delete not possible - no type specified in entity field configuration');
-                return;
-            }
-
-            var entities = $filter("filter")($scope.availableEntities, { Value: itemGuid });
-            var id = entities[0].Id;
-
-            entitiesSvc.tryDeleteAndAskForce(contentType.resolve(), id, entities[0].Text).then(function () {
-                $scope.chosenEntities.splice(index, 1);
-                $scope.maybeReload(true);
-            });
-        };
-
-        // edit needs the Guid - the index isn't important
-        $scope.edit = function (itemGuid, index) {
-            if (itemGuid === null)
-                return alert("no can do"); // todo: i18n
-            var entities = $filter("filter")($scope.availableEntities, { Value: itemGuid });
-            var id = entities[0].Id;
-
-            return eavAdminDialogs.openItemEditWithEntityId(id, $scope.getAvailableEntities);
-        };
-
-        $scope.insertNull = function() {
-            $scope.chosenEntities.push(null);
-        };
-
-        function setDirty() {
-            $scope.form.$setDirty();
-        }
-
         activate();
-    })
-
-    .directive("entityValidation", [function () {
-        return {
-            restrict: "A",
-            require: "?ngModel",
-            link: function(scope, element, attrs, ngModel) {
-                if (!ngModel) return;
-
-                ngModel.$validators.required = function (modelValue, viewValue) {
-                    var value;
-
-                    if (!scope.$parent.$parent.to.required) return true;
-
-                    value = modelValue || viewValue;
-                    if (!value || !Array.isArray(value)) return true;
-                    return value.length > 0;
-                };
-
-                scope.$watch(function () {
-                    return ngModel.$viewValue;
-                }, function (newValue) {
-                    ngModel.$validate();
-                }, true);
-            }
-        };
-    }]);
+    });
